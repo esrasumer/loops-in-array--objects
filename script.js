fadbdd50ccc0790f15301e68ae36bfb716c0c02d
esrasumer <esrasumer28@gmail.com>
2022-06-14 23:47:10 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+ let cars = ['BMV', 'Mercedes', 'Toyota'];
+
+
+ let people = [
+     { firstName: 'Ada', lastName: 'Lovelace' },
+     { firstName: 'sena', lastName: 'love' },
+     { firstName: 'seda', lastName: 'lace' },
+ ]
+
+ //objects
+
+ //  for (let i = 0; i < people.length; i++) {
+ //      console.log(people[i].firstName);
+ //  }
+
+
+ //arrays
+ // for (let i = 0; i < cars.length; i++) {
+ //     console.log(cars[i]);
+ // }
+ //arrays
+
+ //for-in
+
+ //  for (let i in cars) {
+ //      console.log(`index : ${i} value : ${cars[i]}`);
+ //  }
+
+ //object
+
+ //for-each
+
+ //  cars.forEach(function(item) {
+ //      console.log(item);
+ //  });
+
+
+
+
+ // let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+ // for (let i = 1; i < 8; i++) {
+ //     console.log(i);
+ // }
